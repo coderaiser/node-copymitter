e1a61515235cb4ef50ff3fea8e076ff574261775
coderaiser <mnemonic.enemy@gmail.com>
2015-02-12 06:15:10 -0500

    feature(copymitter) add percent

--- bin/copymitter.js
+++ bin/copymitter.js
@@ -3,30 +3,31 @@
 (function() {
     'use strict';
     
-    var cp,
-        copymitor   = require('../lib/copymitor');
+    var copymitor   = require('..');
     
     copy();
     
     function copy() {
-        var from    = '/home/coderaiser/copymitor/node_modules/pipe-io',
-            to      = '/home/coderaiser/copymitor/example';
+        var cp,
+            from    = '/home/coderaiser/copymitter/node_modules/pipe-io',
+            to      = '/home/coderaiser/copymitter/example',
+            files   = [
+                'sldfjdlsfj',
+                '.npmignore',
+                'ChangeLog',
+                'LICENSE',
+                'README.md',
+                'package.json'
+            ];
         
-        cp = copymitor(from, to, [
-            'lib',
-            '.npmignore',
-            'ChangeLog',
-            'LICENSE',
-            'README.md',
-            'package.json'
-        ]);
+        cp = copymitor(from, to, files);
         
-        cp.on('file', function(data) {
-            console.log(data);
+        cp.on('file', function(name, i, percent) {
+            console.log(percent + ' ->', name);
         });
         
-        cp.on('error', function(error, name) {
-            console.error(name, ':', error.message);
+        cp.on('error', function(error, name, i, percent) {
+            console.error(percent + ' ->', name, ':', error.message);
             cp.emit('continue');
         });
         
