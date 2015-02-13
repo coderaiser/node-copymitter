082c0a92bb83854f7b9e7d1b022757a533752103
coderaiser <mnemonic.enemy@gmail.com>
2015-02-13 02:59:53 -0500

    feature(copymitter) add ability to work with absolute path in bin

--- bin/copymitter.js
+++ bin/copymitter.js
@@ -4,17 +4,24 @@
     'use strict';
     
     var copymitor   = require('..'),
+        path        = require('path'),
         glob        = require('glob'),
         args        = process.argv.slice(2),
         arg         = args[0];
         
-    if (/^(-v|--v)$/.test(arg))
+    if (/^-(v|-version)$/.test(arg))
         version();
-    else if (!arg || /^(-h|--help)$/.test(arg))
+    else if (!arg || args.length === 1|| /^-(h|-help)$/.test(arg))
         help();
     else
         glob(arg, function(error, files) {
-            var to      = args[1],
+            var from,
+                to      = args[1],
+                name    = files[0];
+            
+            if (~name.indexOf('/'))
+                from    = path.dirname(name);
+            else
                 from    = process.cwd();
             
             if (error)
@@ -35,6 +42,10 @@
         
         cp.on('progress', function(percent) {
             console.log(percent);
+        });
+        
+        cp.on('end', function() {
+            console.log('Done.');
         });
     }
     
