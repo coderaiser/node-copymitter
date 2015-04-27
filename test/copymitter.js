7abd29109c17ed90dcae1fcc38257beb255b7ce3
coderaiser <mnemonic.enemy@gmail.com>
2015-04-27 11:09:23 -0400

    test(copymitter) folder: error EACESS

--- test/copymitter.js
+++ test/copymitter.js
@@ -5,9 +5,27 @@
         test        = require('tape'),
         copymitter  = require('..');
     
-    test('file -> file: error EACESS', function(t) {
+    test('file: error EACESS', function(t) {
         var cp = copymitter(__dirname, '/', [
             path.basename(__filename)
+        ]);
+        
+        cp.on('error', function(error) {
+            t.equal(error.code, 'EACCES', error.message);
+            cp.abort();
+        });
+        
+        cp.on('end', function() {
+            t.end();
+        });
+    });
+    
+    test('folder: error EACESS', function(t) {
+        var from    = path.join(__dirname, '..'),
+            name    = path.basename(__dirname);
+        
+        var cp = copymitter(from, '/', [
+            name
         ]);
         
         cp.on('error', function(error) {
