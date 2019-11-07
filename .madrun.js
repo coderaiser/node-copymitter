105a2c1f49819b34e8009a6234bb150d68159532
coderaiser <coderaiser@cloudcmd.io>
2019-11-07 16:57:52 +0200

    feature(package) madrun v5.0.0

--- .madrun.js
+++ .madrun.js
@@ -9,7 +9,7 @@ module.exports = {
     'test': () => 'tape test/*.js',
     'coverage': () => 'nyc npm test',
     'report': () => 'nyc report --reporter=text-lcov | coveralls',
-    'lint': () => 'putout lib test madrun.js',
+    'lint': () => 'putout lib test .madrun.js',
     'fix:lint': () => run('lint', '--fix'),
 };
 
